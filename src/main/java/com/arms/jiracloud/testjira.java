8d9c760718c5f075f493a52c0f84da05cdade563
chanho470 <chanho470@naver.com>
2023-07-18 21:30:02 +0900

    feat: [JAVA-1] #jira cloud 토큰인증  특정이슈 조회 - 23.07.18
    #close #time 1h +review SR @313devops
    
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::chanho470 ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/3

---
+++ src/main/java/com/arms/jiracloud/testjira.java
@@ -0,0 +1,62 @@
+package com.arms.jiracloud;
+
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import java.util.Base64;
+@Component
+
+public class testjira {
+
+    private static final String username = "chanho9611@gmail.com";
+    private static final String password = "ATATT3xFfGF0iohROaJjDtf-me3WtnFFjhwJAZJmNp4ricHtSQkJhoQ3quKSv615E-f__pVkO8jKGUVndcufhgmfdjkcTOdLafNIHABGCtyQp705Na9j4zS7Pw-iTXeo-81WsoWkHHk9dUu6DBMB4CGoZAAET_G2SUFKibgMVkB4U7oYaQQrUhs=EC1DE5C6";
+    private static final String jiraBaseURL = "https://testchkim.atlassian.net/rest/api/3/";
+    private RestTemplate restTemplate;
+    private HttpHeaders httpHeaders;
+
+    public testjira() {
+        restTemplate = new RestTemplate();
+        httpHeaders = createHeadersWithAuthentication();
+    }
+
+    private HttpHeaders createHeadersWithAuthentication() {
+        String plainCreds = username + ":" + password;
+        byte[] base64CredsBytes = Base64.getEncoder().encode(plainCreds.getBytes());
+        String base64Creds = new String(base64CredsBytes);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Creds);
+
+        return headers;
+    }
+
+    public ResponseEntity getIssue(String issueId) {
+        String url = jiraBaseURL + "issue/" + issueId;
+
+        HttpEntity<?> requestEntity = new HttpEntity(httpHeaders);
+        return restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
+    }
+
+    public ResponseEntity createIssue(String key, String summary, String description, String issueType) {
+        String createIssueJSON = createCreateIssueJSON(key, summary, description, issueType);
+
+        String url = jiraBaseURL + "issue";
+
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> requestEntity = new HttpEntity<String>(createIssueJSON, httpHeaders);
+
+        return restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+
+    }
+
+    private String createCreateIssueJSON(String key, String summary, String description, String issueType) {
+        String createIssueJSON = "{\"fields\":{\"project\":{\"key\":\"$KEY\"},\"summary\":\"$SUMMARY\",\"description\":\"$DESCRIPTION\",\"issuetype\": {\"name\": \"$ISSUETYPE\"}}}";
+
+        createIssueJSON = createIssueJSON.replace("$KEY", key);
+        createIssueJSON = createIssueJSON.replace("$SUMMARY", summary);
+        createIssueJSON = createIssueJSON.replace("$DESCRIPTION", description);
+        return createIssueJSON.replace("$ISSUETYPE", issueType);
+    }
+
+}

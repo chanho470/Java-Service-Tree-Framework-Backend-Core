59aea923cba96b3dfd4128236362f9b6e8f10f9a
chanho470 <chanho470@naver.com>
2023-07-19 01:38:10 +0900

    feat: [JAVA-1] #jira cloud OAuth2.0 인증 방법 시도 - 23.07.19
    파악내용(파악중)
    참조문서
    https://developer.atlassian.com/cloud/jira/platform/oauth-2-3lo-apps/
    - 지라 OAuth 앱에 컬백 url 등록해야함 -> 인증 url 생성됨
    - 컬백 url로 지라 인증서버에 요청하면 api 사용가능 토큰을 생성해주는 것으로 파악됨
    
    발생이슈
    - 인증url을 로컬 프로젝트에서 쏘면 리턴값으로 로그인페이지가 뜸
    - 로컬 환경에서 쏘면 인증 창이 뜸...
    
    추후 확인 필요 내용
    - 컬백 url에 넣는 값 확인필요
    - 전체 인증 메카니즘 다시 파악 필요
    - 토큰 인증기간 이슈 컨트롤 가능한것으로 파악됨

---
+++ src/main/java/com/arms/jiracloud/OAuth2jira.java
@@ -0,0 +1,22 @@
+package com.arms.jiracloud;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class OAuth2jira {
+    private RestTemplate restTemplate;
+
+    private static final String autorizationUrl = "https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=mf9ZDzYl5r0LqO2r4ojp7LLL9EGRKrGa&scope=read%3Ajira-work%20manage%3Ajira-project%20write%3Ajira-work&redirect_uri=http%3A%2F%2Flocalhost%3A31313%2Farms%2FjiraCloud%2FCallback&state=chkim&response_type=code&prompt=consent";
+    public OAuth2jira() {
+        restTemplate = new RestTemplate();
+        //httpHeaders = requestautorizationUrl();
+    }
+
+    public String requestautorizationUrl(){
+        ResponseEntity<String> response = restTemplate.exchange(autorizationUrl, HttpMethod.GET, null, String.class);
+        System.out.println("인증 URL 리턴22222222"+response);
+        String responseBody = response.getBody();
+        return responseBody;
+    }
+}
--- src/main/java/com/arms/jiracloud/controller/JiraCloudController.java
+++ src/main/java/com/arms/jiracloud/controller/JiraCloudController.java
@@ -1,6 +1,7 @@
 package com.arms.jiracloud.controller;
 
 
+import com.arms.jiracloud.OAuth2jira;
 import com.arms.jiracloud.testjira;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 @Slf4j
 @Controller
 @RequestMapping(value = {"/arms/jiraCloud"})
@@ -16,10 +22,13 @@ public class JiraCloudController {
 
     @Autowired
     private final testjira jiraClient;
+    @Autowired
+    private final OAuth2jira oauthClient;
 
     @Autowired
-    public JiraCloudController(testjira jiraClient) {
+    public JiraCloudController(testjira jiraClient,OAuth2jira  oauthClient) {
         this.jiraClient = jiraClient;
+        this.oauthClient=oauthClient;
     }
 
     @GetMapping("GetIssue")
@@ -38,4 +47,14 @@ public class JiraCloudController {
         String issueType = "Bug";
         return jiraClient.createIssue(key, summary, description, issueType);
     }
+
+
+    @GetMapping("Callback")
+    public void requestautorizationUrl(HttpServletResponse response)throws IOException {
+        String body = oauthClient.requestautorizationUrl();
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println(body);
+    }
+
 }
